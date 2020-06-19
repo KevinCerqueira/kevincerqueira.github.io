9d727a2b3dff4456ee3e3578cbe8722d9fa6b5d1
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2020-06-18 21:26:08 -0300

    :books: add SweetAlert 2

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ $('a').click(function() {
     }, 900);
     return false;
 });
+$(document).ready(function () {
+    Swal.fire({
+        title: '<strong>ATENÇÃO</strong>',
+        icon: 'warning',
+        html:
+          'Este site ainda está em fase de ' +
+          '<strong>construção</strong>! ' +
+          'Então é possível encontrar resquícios '+
+          'expostos da programação.',
+        showCloseButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> Entendi!',
+        confirmButtonAriaLabel: 'Thumbs up, great!'
+      });
+});
