b91aea27dd7fc376d98a9dffd91ab817c23c6fcb
Kevin Cerqueira <knbassbr@gmail.com>
2021-05-17 10:24:33 -0300

    Revert ":fire: cleaning"
    This reverts commit c0bb7d02027e165575ec1cef5ce085f7bdc17589.

---
+++ README.md
@@ -0,0 +1,2 @@
+# [KevinCerqueira.github.io](https://kevincerqueira.github.io/)
+## My personal GitHub Page.
---
+++ js/app.js
@@ -0,0 +1,22 @@
+$('a').click(function() {
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 900);
+    return false;
+});
+// $(document).ready(function () {
+//     Swal.fire({
+//         title: '<strong>ATENÇÃO</strong>',
+//         icon: 'warning',
+//         html:
+//           'Este site ainda está em fase de ' +
+//           '<strong>construção</strong>! ' +
+//           'Então é possível encontrar algumas '+
+//           'coisas que ainda não estão 100% desenvolvidas.',
+//         showCloseButton: false,
+//         focusConfirm: false,
+//         confirmButtonText:
+//           '<i class="fa fa-thumbs-up"></i> Entendi!',
+//         confirmButtonAriaLabel: ''
+//       });
+// });
