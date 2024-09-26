9a067b1cc55ccb983fb2571340b05fb0466236f6
Kevin Cerqueira <38994372+KevinCerqueira@users.noreply.github.com>
2024-09-26 11:45:40 -0300

    starting

---
+++ src/pages/firstPage.php
@@ -0,0 +1,10 @@
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      <img id="perfil" class="img-fluid" src="style/perfil.jpg" alt="Perfil">
+    </div>
+  </div>
+</div>
